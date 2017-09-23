52560f9f17d7291bcfa28292010e59e776144aae
Sylvestre <sylvestre.cao@gmail.com>
2017-09-23 17:51:56 +0200

    clean code, add new pages

--- src/CathayBundle/Controller/FrontController.php
+++ src/CathayBundle/Controller/FrontController.php
@@ -8,11 +8,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class FrontController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      * @Method("GET")
      */
     public function indexAction()
     {
         return $this->render('CathayBundle:Front:index.html.twig');
     }
+
+    /**
+     * @Route("/contactez-nous", name="contact")
+     * @Method("GET")
+     */
+    public function contactAction()
+    {
+        return $this->render('CathayBundle:Front:contact.html.twig');
+    }
+
+    /**
+     * @Route("/commander", name="order")
+     * @Method("GET")
+     */
+    public function orderAction()
+    {
+       return $this->render('CathayBundle:Front:order.html.twig');
+    }
+
+    /**
+     * @Route("/univers/electronique", name="electronic_universe")
+     * @Method("GET")
+     */
+    public function electronicUniverseAction()
+    {
+        return $this->render('CathayBundle:Front:electronic-universe.html.twig');
+    }
+    
 }
---
+++ web/js/main.js
@@ -0,0 +1,13 @@
+jQuery(function($) {'use strict',
+
+	//#main-slider
+	$(function(){
+		$('#main-slider.carousel').carousel({
+			interval: 8000
+		});
+	});
+
+	//Initiat WOW JS
+	new WOW().init();
+
+});
